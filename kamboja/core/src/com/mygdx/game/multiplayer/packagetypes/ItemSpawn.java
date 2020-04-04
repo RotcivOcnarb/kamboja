6c95f8c155829e1c052054fdb387833c5b4f3000
Rotciv Ocnarb <rotciv_ocnarb@hotmail.com>
2020-04-04 18:11:35 -0300

    seri

--- kamboja/core/src/com/mygdx/game/multiplayer/packagetypes/ItemSpawn.java
+++ kamboja/core/src/com/mygdx/game/multiplayer/packagetypes/ItemSpawn.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.multiplayer.packagetypes;
 
+import java.io.Serializable;
+
 import com.badlogic.gdx.math.Vector2;
 
-public class ItemSpawn {
+public class ItemSpawn implements Serializable{
 
 	public Vector2 position;
 	public int id;
