37a21eacd9cec6efce5c96f79e2d605db41210c2
Rotciv Ocnarb <rotciv_ocnarb@hotmail.com>
2020-04-04 17:48:24 -0300

    lerp multiplayer position

--- kamboja/core/src/com/mygdx/game/objects/players/MultiplayerPlayer.java
+++ kamboja/core/src/com/mygdx/game/objects/players/MultiplayerPlayer.java
@@ -1,11 +1,15 @@
 package com.mygdx.game.objects.players;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.mygdx.game.states.GameState;
 
 public class MultiplayerPlayer extends Player{
 
 	public String ip;
+	
+	Vector2 targetPosition;
+	Vector2 targetAngle;
 	
 	public MultiplayerPlayer(Body body, int id, GameState state, String name, String ip) {
 		super(body, id, state, name);
@@ -15,6 +19,18 @@ public class MultiplayerPlayer extends Player{
 	@Override
 	public void update(float delta) {
 		nonInputUpdate(delta);
+		
+		if(targetPosition != null)
+			body.applyForceToCenter(body.getWorldCenter().cpy().sub(targetPosition), true);
+		
+		if(targetAngle != null)
+			nextAngle = targetAngle;
+	}
+	
+	@Override
+	public void updateTransform(Vector2 position, Vector2 angle) {
+		this.targetPosition = position;
+		this.targetAngle = angle;
 	}
 	
 }
